88636513c9571f707350f0a14da9aae8166a7b54
dank.zheng <dank.zheng@mplus.tech>
2023-08-25 07:45:25 +0800

    new handlers

---
+++ handlers/json-schema.go
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Version: 
+ * @Company: Dank
+ * @Author: dank.zheng
+ * @Date: 2023-08-25 07:01:13
+ * @LastEditors: dank.zheng
+ * @LastEditTime: 2023-08-25 07:05:02
+ */
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+function GetJsonSchema(c *gin.Context) {
+	
+}
---
+++ handlers/uploadfiles.go
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Version: 
+ * @Company: Dank
+ * @Author: dank.zheng
+ * @Date: 2023-08-25 07:00:23
+ * @LastEditors: dank.zheng
+ * @LastEditTime: 2023-08-25 07:04:35
+ */
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+func UploadFiles(c *gin.Context) {
+	
+}
